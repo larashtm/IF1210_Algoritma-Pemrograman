813599cb800e2cc09e8569999bd199f8c3d77853
larashtm <19623239@std.stei.itb.ac.id>
2026-03-16 04:27:38 +0700

    feat: add luaslingkaran dan penjumlahan program

---
+++ Praktikum/Praktikum0/luaslingkaran.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    float r, l;
+    const float PI = 3.1415;
+    scanf("%f", &r);
+    l = PI * r * r;
+    printf("%f\n", l);
+    return 0;
+}
---
+++ Praktikum/Praktikum0/penjumlahan.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    float a, b, c;
+    float j;
+
+    scanf("%f", &a);
+    scanf("%f", &b);
+    scanf("%f", &c);
+
+    j = a + b + c;
+
+    printf("%f\n", j);
+
+    return 0;
+}
